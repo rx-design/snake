1ad3435d5fc662a854e10243f9175fb70c3e2adf
KingCrimsonN <MSUA1307@gmail.com>
2023-04-23 21:12:36 +0300

    Added the health and points game logic, food types

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -2,49 +2,51 @@ using UnityEngine;
 
 public class Food : MonoBehaviour
 {
-    public BoxCollider2D gridArea;
-    public Snake snake;
+  public BoxCollider2D gridArea;
+  public Snake snake;
 
-    private void Start()
+  public bool good;
+
+  private void Start()
+  {
+    RandomizePosition();
+  }
+
+  private void RandomizePosition()
+  {
+    var bounds = gridArea.bounds;
+
+    while (true)
     {
-        RandomizePosition();
-    }
+      var x = Random.Range(bounds.min.x, bounds.max.x);
+      var y = Random.Range(bounds.min.y, bounds.max.y);
 
-    private void RandomizePosition()
+      var position = new Vector3(
+          Mathf.Round(x),
+          Mathf.Round(y),
+          0.0f
+      );
+
+      if (!IsValidPosition(position))
+      {
+        continue;
+      }
+
+      transform.position = position;
+      break;
+    }
+  }
+
+  private bool IsValidPosition(Vector3 position)
+  {
+    return !snake.Occupies(position);
+  }
+
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.CompareTag("Player"))
     {
-        var bounds = gridArea.bounds;
-
-        while (true)
-        {
-            var x = Random.Range(bounds.min.x, bounds.max.x);
-            var y = Random.Range(bounds.min.y, bounds.max.y);
-
-            var position = new Vector3(
-                Mathf.Round(x),
-                Mathf.Round(y),
-                0.0f
-            );
-
-            if (!IsValidPosition(position))
-            {
-                continue;
-            }
-
-            transform.position = position;
-            break;
-        }
+      RandomizePosition();
     }
-
-    private bool IsValidPosition(Vector3 position)
-    {
-        return !snake.Occupies(position);
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            RandomizePosition();
-        }
-    }
+  }
 }
---
+++ Assets/Scripts/PointsManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PointsManager : MonoBehaviour
+{
+
+  public TMP_Text health;
+
+  public TMP_Text points;
+
+  public void UpdateHealth()
+  {
+    health.text = "Health: " + GameManager.health.ToString();
+  }
+
+  public void UpdatePoints()
+  {
+    points.text = "Points: " + GameManager.points.ToString();
+  }
+
+}
