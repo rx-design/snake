3044c5b6266210ae38b70766cf8981faa04c2591
KingCrimsonN <48062712+KingCrimsonN@users.noreply.github.com>
2023-04-24 10:20:01 +0300

    Added the health and points game logic, food types (#2)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameManager
+{
+  private const int startingHealth = 5;
+
+  public static int health = startingHealth;
+
+  public static int points = 0;
+
+  public static void RemoveHealth()
+  {
+    health--;
+    Debug.Log(health);
+  }
+
+  public static void AddPoints()
+  {
+    points++;
+    Debug.Log(points);
+  }
+
+  public static void StartOver()
+  {
+    health = startingHealth;
+    points = 0;
+  }
+}
