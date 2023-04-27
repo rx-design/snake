e5544ad8d997a198d3c3f19d799173958f6e6df5
MrNeal01 <79901150+MrNeal01@users.noreply.github.com>
2023-04-27 15:19:35 +0300

    Game over UI (#3)

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -4,135 +4,168 @@ using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
-  public Transform segmentPrefab;
-  public int initialSize = 4;
-  public float speed = 20.0f;
-  public float speedMultiplier = 1.0f;
+    public Transform segmentPrefab;
+    public int initialSize = 4;
+    public float speed = 1.0f;
+    public float speedMultiplier = 1.0f;
 
-  private readonly List<Transform> _segments = new List<Transform>();
-  private Vector2 _input;
-  private Vector2 _direction = Vector2.right;
-  private float _nextUpdate;
+    private readonly List<Transform> _segments = new List<Transform>();
+    private Vector2 _input;
+    private Vector2 _direction = Vector2.right;
+    private float _nextUpdate;
+    private bool _gameOver;
 
-  [SerializeField]
-  private PointsManager pointsManager;
+    [SerializeField]
+    private PointsManager pointsManager;
 
-  private void Start()
-  {
-    ResetState();
-    pointsManager.UpdateHealth();
-  }
+    public Canvas gameOverCanvas;
 
-  private void Update()
-  {
-    if (_direction.x != 0.0f)
+    private void Start()
     {
-      if (Input.GetKeyDown(KeyCode.W))
-      {
-        _input = Vector2.up;
-      }
-      else if (Input.GetKeyDown(KeyCode.S))
-      {
-        _input = Vector2.down;
-      }
-    }
-    else if (_direction.y != 0.0f)
-    {
-      if (Input.GetKeyDown(KeyCode.A))
-      {
-        _input = Vector2.left;
-      }
-      else if (Input.GetKeyDown(KeyCode.D))
-      {
-        _input = Vector2.right;
-      }
-    }
-  }
-
-  private void FixedUpdate()
-  {
-    if (Time.time < _nextUpdate)
-    {
-      return;
-    }
-
-    if (_input != Vector2.zero)
-    {
-      _direction = _input;
-    }
-
-    for (var i = _segments.Count - 1; i > 0; i--)
-    {
-      _segments[i].position = _segments[i - 1].position;
-    }
-
-    var position = transform.position;
-
-    transform.position = new Vector3(
-        Mathf.Round(position.x) + _direction.x,
-        Mathf.Round(position.y) + _direction.y,
-        0.0f
-    );
-
-    _nextUpdate = Time.time + 1.0f / (speed * speedMultiplier);
-  }
-
-  public bool Occupies(Vector3 position)
-  {
-    return _segments
-        .Any(s => s.position.Equals(position));
-  }
-
-  private void Grow()
-  {
-    var segment = Instantiate(segmentPrefab);
-    segment.position = _segments.Last().position;
-
-    _segments.Add(segment);
-  }
-
-  private void ResetState()
-  {
-    GameManager.StartOver();
-    for (var i = 1; i < _segments.Count; i++)
-    {
-      Destroy(_segments[i].gameObject);
-    }
-
-    _segments.Clear();
-    _segments.Add(transform);
-
-    for (var i = 1; i < initialSize; i++)
-    {
-      _segments.Add(Instantiate(segmentPrefab));
-    }
-
-    transform.position = Vector3.zero;
-  }
-
-  private void OnTriggerEnter2D(Collider2D other)
-  {
-    if (other.CompareTag("Food"))
-    {
-      Food f = other.gameObject.GetComponent<Food>();
-      if (f.good)
-      {
-        Grow();
-        GameManager.AddPoints();
-        pointsManager.UpdatePoints();
-        return;
-      }
-      GameManager.RemoveHealth();
-      pointsManager.UpdateHealth();
-      if (GameManager.health <= 0)
-      {
         ResetState();
         pointsManager.UpdateHealth();
-      }
+        gameOverCanvas.gameObject.SetActive(false);
     }
-    else if (other.CompareTag("Obstacle"))
+
+    private void Update()
     {
-      ResetState();
+        if (_gameOver)
+        {
+            return;
+        }
+
+        if (_direction.x != 0.0f)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                _input = Vector2.up;
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                _input = Vector2.down;
+            }
+        }
+        else if (_direction.y != 0.0f)
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                _input = Vector2.left;
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                _input = Vector2.right;
+            }
+        }
     }
-  }
+
+    private void FixedUpdate()
+    {
+        if (_gameOver)
+        {
+            return;
+        }
+
+        if (_input != Vector2.zero)
+        {
+            _direction = _input;
+        }
+
+        for (var i = _segments.Count - 1; i > 0; i--)
+        {
+            _segments[i].position = _segments[i - 1].position;
+        }
+
+        var position = transform.position;
+
+        transform.position = new Vector3(
+            Mathf.Round(position.x) + _direction.x,
+            Mathf.Round(position.y) + _direction.y,
+            0.0f
+        );
+
+        _nextUpdate = Time.time + 1.0f / (speed * speedMultiplier);
+    }
+
+    public bool Occupies(Vector3 position)
+    {
+        return _segments
+            .Any(s => s.position.Equals(position));
+    }
+
+    public void RestartGame()
+    {
+        ResetState();
+        gameOverCanvas.gameObject.SetActive(false);
+    }
+
+
+    private void Grow()
+    {
+        var segment = Instantiate(segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+
+        _segments.Add(segment);
+    }
+
+
+    private void ResetState()
+    {
+        GameManager.StartOver();
+        for (var i = 1; i < _segments.Count; i++)
+        {
+            Destroy(_segments[i].gameObject);
+        }
+
+        _segments.Clear();
+        _segments.Add(transform);
+
+        for (var i = 1; i < initialSize; i++)
+        {
+            _segments.Add(Instantiate(segmentPrefab));
+        }
+
+        transform.position = Vector3.zero;
+
+        // Reset health and points
+        GameManager.health = 5;
+        GameManager.points = 0;
+        pointsManager.UpdateHealth();
+        pointsManager.UpdatePoints();
+
+        _gameOver = false;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (_gameOver)
+        {
+            return;
+        }
+
+        if (other.CompareTag("Food"))
+        {
+            Food f = other.gameObject.GetComponent<Food>();
+            if (f.good)
+            {
+                Grow();
+                GameManager.AddPoints();
+                pointsManager.UpdatePoints();
+                return;
+            }
+            GameManager.RemoveHealth();
+            pointsManager.UpdateHealth();
+            if (GameManager.health <= 0)
+            {
+                _gameOver = true;
+                gameOverCanvas.gameObject.SetActive(true);
+            }
+        }
+        else if (other.CompareTag("Obstacle"))
+        {
+            _gameOver = true;
+            gameOverCanvas.gameObject.SetActive(true);
+        }
+    }
 }
+
