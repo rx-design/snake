fb7a26393ececab382bd178942e5acfc7746c256
pjotr.surkov <pjotr.surkov@bigbank.ee>
2023-05-11 14:05:04 +0300

    Add snake rotation

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -6,15 +6,24 @@ using UnityEngine.Events;
 public class Snake : MonoBehaviour
 {
     public static readonly UnityEvent IsDied = new();
-    public Transform segmentPrefab;
+    public SnakeSegment segmentPrefab;
     public int initialSize = 4;
     public float speed = 20.0f;
     public float speedMultiplier = 1.0f;
 
-    private readonly List<Transform> _segments = new();
+    private readonly List<SnakeSegment> _segments = new();
+    private SnakeSegment _head;
     private Vector2 _input;
-    private Vector2 _direction = Vector2.right;
     private float _nextUpdate;
+
+    private void Awake()
+    {
+        _head = GetComponent<SnakeSegment>();
+
+        if (_head != null) return;
+        _head = gameObject.AddComponent<SnakeSegment>();
+        _head.hideFlags = HideFlags.HideInInspector;
+    }
 
     private void OnEnable()
     {
@@ -23,7 +32,7 @@ public class Snake : MonoBehaviour
 
     private void Update()
     {
-        if (_direction.x != 0.0f)
+        if (_head.Direction.x != 0.0f)
         {
             if (Input.GetKeyDown(KeyCode.W))
             {
@@ -34,7 +43,7 @@ public class Snake : MonoBehaviour
                 _input = Vector2.down;
             }
         }
-        else if (_direction.y != 0.0f)
+        else if (_head.Direction.y != 0.0f)
         {
             if (Input.GetKeyDown(KeyCode.A))
             {
@@ -56,19 +65,20 @@ public class Snake : MonoBehaviour
 
         if (_input != Vector2.zero)
         {
-            _direction = _input;
+            _head.SetDirection(_input, Vector2.zero);
         }
 
         for (var i = _segments.Count - 1; i > 0; i--)
         {
-            _segments[i].position = _segments[i - 1].position;
+            _segments[i].Follow(_segments[i - 1], i, _segments.Count);
         }
 
-        var position = transform.position;
+        var direction = _head.Direction;
+        var position = _head.transform.position;
 
-        transform.position = new Vector3(
-            Mathf.Round(position.x) + _direction.x,
-            Mathf.Round(position.y) + _direction.y,
+        _head.transform.position = new Vector3(
+            Mathf.Round(position.x) + direction.x,
+            Mathf.Round(position.y) + direction.y,
             0.0f
         );
 
@@ -78,26 +88,29 @@ public class Snake : MonoBehaviour
     public bool Occupies(Vector3 position)
     {
         return _segments
-            .Any(s => s.position.Equals(position));
+            .Any(s => s.transform.position.Equals(position));
     }
 
     private void Grow()
     {
         var segment = Instantiate(segmentPrefab);
-        segment.position = _segments.Last().position;
+        segment.Follow(_segments.Last(), _segments.Count, _segments.Count + 1);
 
         _segments.Add(segment);
     }
 
     private void ResetState()
     {
+        _head.SetDirection(Vector2.right, Vector2.zero);
+        _head.transform.position = Vector3.zero;
+
         for (var i = 1; i < _segments.Count; i++)
         {
             Destroy(_segments[i].gameObject);
         }
 
         _segments.Clear();
-        _segments.Add(transform);
+        _segments.Add(_head);
 
         for (var i = 1; i < initialSize; i++)
         {
