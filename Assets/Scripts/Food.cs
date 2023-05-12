72d5317bfbfca66e84bafb64f956db701c5fce78
KingCrimsonN <48062712+KingCrimsonN@users.noreply.github.com>
2023-05-12 10:21:23 +0300

    Snake and letters (#10)

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -1,15 +1,22 @@
 using Enums;
 using UnityEngine;
 using UnityEngine.Events;
+using TMPro;
 
 public class Food : MonoBehaviour
 {
-    public static readonly UnityEvent<Food> IsTaken = new();
-    public Letter letter;
+  public static readonly UnityEvent<Food> IsTaken = new();
+  public Letter letter;
+  public TMP_Text letterDisplay;
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (!other.CompareTag("Player")) return;
-        IsTaken?.Invoke(this);
-    }
+  void Start()
+  {
+    letterDisplay.text = letter.ToString();
+  }
+
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    if (!other.CompareTag("Player")) return;
+    IsTaken?.Invoke(this);
+  }
 }
