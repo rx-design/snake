19038405d8fb5cbe2cdf661d5e1c4392904b9f5b
pjotr.surkov <pjotr.surkov@bigbank.ee>
2023-05-13 00:06:58 +0300

    Cleanup

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,132 +8,133 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-  public static readonly UnityEvent<int> LivesUpdated = new();
-  public static readonly UnityEvent<int> ScoreUpdated = new();
-  public static readonly UnityEvent<char[], char[]> CharsUpdated = new();
-  public static readonly UnityEvent GameStarted = new();
-  public static readonly UnityEvent<Result, int> GameEnded = new();
-  public static readonly UnityEvent<string> WordHintUpdated = new UnityEvent<string>();
-  public string[] dialogue;
-  public int startingLives = 5;
-  public List<Word> words;
+    public static GameManager instance;
+    public static readonly UnityEvent<int> LivesUpdated = new();
+    public static readonly UnityEvent<int> ScoreUpdated = new();
+    public static readonly UnityEvent<char[], char[]> CharsUpdated = new();
+    public static readonly UnityEvent GameStarted = new();
+    public static readonly UnityEvent<Result, int> GameEnded = new();
+    public static readonly UnityEvent<string> WordHintUpdated = new();
+    public string[] dialogue;
+    public int startingLives = 5;
+    public List<Word> words;
 
-  private int _lives;
-  private int _score;
-  private char[] _chars;
-  private int _currentLevel = 1;
-  private int _scoreAtLevelStart;
-  private bool _dialogueShown;
+    private char[] _chars;
+    private int _currentLevel = 1;
+    private bool _dialogueShown;
+    private int _lives;
+    private int _score;
+    private int _scoreAtLevelStart;
 
-  private void OnEnable()
-  {
-    Food.IsTaken.AddListener(OnFoodIsTaken);
-    Snake.IsDied.AddListener(() => OnGameOver(Result.Lose));
-  }
-
-  public void Start()
-  {
-    var word = GetWord();
-    _lives = startingLives;
-    _chars = word.chars.ToList().Select(_ => '_').ToArray();
-    _scoreAtLevelStart = _score;
-
-    LivesUpdated?.Invoke(_lives);
-    ScoreUpdated?.Invoke(_score);
-    CharsUpdated?.Invoke(word.chars, _chars);
-    WordHintUpdated?.Invoke(word.hint); // New line
-    GameStarted?.Invoke();
-
-    if (_dialogueShown) return;
-    Time.timeScale = 0.0f;
-    DialogueManager.instance.StartDialogue(dialogue);
-    _dialogueShown = true;
-  }
-
-
-  public void RestartLevel()
-  {
-    _score = _scoreAtLevelStart; // Revert score to the level start
-    Start();
-  }
-
-  public void LoadNextLevel()
-  {
-    _currentLevel++;
-    if (_currentLevel > words.Count) return;
-    WordHintUpdated?.Invoke(words[_currentLevel - 1].hint); // New line
-    Start();
-  }
-
-
-  public void GoToMainMenu()
-  {
-    SceneManager.LoadScene("MainMenu");
-  }
-
-  private void DecreaseLives()
-  {
-    _lives--;
-  }
-
-  private void IncreaseScore()
-  {
-    _score++;
-  }
-
-  private Word GetWord()
-  {
-    return words[_currentLevel - 1];
-  }
-
-  private void OnGameOver(Result result)
-  {
-    Time.timeScale = 0.0f;
-    GameEnded?.Invoke(result, _score);
-  }
-
-  private bool CheckLetter(Letter letter)
-  {
-    var word = GetWord();
-
-    for (var i = 0; i < word.chars.Length; i++)
+    public void Awake()
     {
-      if (word.chars[i] != (char)letter || _chars[i] != '_') continue;
-      _chars[i] = (char)letter;
-      CharsUpdated?.Invoke(word.chars, _chars);
-
-      return true;
+        instance = this;
     }
 
-    CharsUpdated?.Invoke(word.chars, _chars);
-
-    return false;
-  }
-
-  private void OnFoodIsTaken(Food food)
-  {
-    if (CheckLetter(food.letter))
+    public void Start()
     {
-      IncreaseScore();
-      ScoreUpdated?.Invoke(_score);
+        var word = GetWord();
+        _lives = startingLives;
+        _chars = word.chars.ToList().Select(_ => '_').ToArray();
+        _scoreAtLevelStart = _score;
 
-      if (_chars.Contains('_')) return;
+        LivesUpdated?.Invoke(_lives);
+        ScoreUpdated?.Invoke(_score);
+        CharsUpdated?.Invoke(word.chars, _chars);
+        WordHintUpdated?.Invoke(word.hint);
+        GameStarted?.Invoke();
 
-      var result = _currentLevel < words.Count
-          ? Result.Win
-          : Result.GameWin;
-
-      OnGameOver(result);
-
-      return;
+        if (_dialogueShown) return;
+        DialogueManager.instance.StartDialogue(dialogue);
+        _dialogueShown = true;
     }
 
-    DecreaseLives();
-    LivesUpdated?.Invoke(_lives);
-
-    if (_lives <= 0)
+    private void OnEnable()
     {
-      OnGameOver(Result.Lose);
+        Food.IsTaken.AddListener(OnFoodIsTaken);
+        Snake.IsDied.AddListener(() => OnGameOver(Result.Lose));
     }
-  }
+
+
+    public void RestartLevel()
+    {
+        _score = _scoreAtLevelStart;
+        Start();
+    }
+
+    public void LoadNextLevel()
+    {
+        _currentLevel++;
+        if (_currentLevel > words.Count) return;
+        WordHintUpdated?.Invoke(words[_currentLevel - 1].hint);
+        Start();
+    }
+
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    private void DecreaseLives()
+    {
+        _lives--;
+    }
+
+    private void IncreaseScore()
+    {
+        _score++;
+    }
+
+    private Word GetWord()
+    {
+        return words[_currentLevel - 1];
+    }
+
+    private void OnGameOver(Result result)
+    {
+        GameEnded?.Invoke(result, _score);
+    }
+
+    private bool CheckLetter(Letter letter)
+    {
+        var word = GetWord();
+
+        for (var i = 0; i < word.chars.Length; i++)
+        {
+            if (word.chars[i] != (char)letter || _chars[i] != '_') continue;
+            _chars[i] = (char)letter;
+            CharsUpdated?.Invoke(word.chars, _chars);
+
+            return true;
+        }
+
+        CharsUpdated?.Invoke(word.chars, _chars);
+
+        return false;
+    }
+
+    private void OnFoodIsTaken(Food food)
+    {
+        if (CheckLetter(food.letter))
+        {
+            IncreaseScore();
+            ScoreUpdated?.Invoke(_score);
+
+            if (_chars.Contains('_')) return;
+
+            var result = _currentLevel < words.Count
+                ? Result.Win
+                : Result.GameWin;
+
+            OnGameOver(result);
+
+            return;
+        }
+
+        DecreaseLives();
+        LivesUpdated?.Invoke(_lives);
+
+        if (_lives <= 0) OnGameOver(Result.Lose);
+    }
 }
--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -5,131 +5,101 @@ using UnityEngine.Events;
 
 public class Snake : MonoBehaviour
 {
-  public static readonly UnityEvent IsDied = new();
-  public SnakeSegment segmentPrefab;
-  public int initialSize = 4;
-  public float speed = 20.0f;
-  public float speedMultiplier = 1.0f;
+    public static readonly UnityEvent IsDied = new();
+    public SnakeSegment segmentPrefab;
+    public int initialSize = 4;
+    public float speed = 20.0f;
+    public float speedMultiplier = 1.0f;
 
-  private readonly List<SnakeSegment> _segments = new();
-  private SnakeSegment _head;
-  private Vector2 _input;
-  private float _nextUpdate;
+    private readonly List<SnakeSegment> _segments = new();
+    private SnakeSegment _head;
+    private Vector2 _input;
+    private float _nextUpdate;
 
-  private void Awake()
-  {
-    _head = GetComponent<SnakeSegment>();
-
-    if (_head != null) return;
-    _head = gameObject.AddComponent<SnakeSegment>();
-    _head.hideFlags = HideFlags.HideInInspector;
-  }
-
-  private void OnEnable()
-  {
-    GameManager.GameStarted.AddListener(ResetState);
-  }
-
-  private void Update()
-  {
-    if (_head.Direction.x != 0.0f)
+    private void Awake()
     {
-      if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-      {
-        _input = Vector2.up;
-      }
-      else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-      {
-        _input = Vector2.down;
-      }
-    }
-    else if (_head.Direction.y != 0.0f)
-    {
-      if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-      {
-        _input = Vector2.left;
-      }
-      else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-      {
-        _input = Vector2.right;
-      }
-    }
-  }
+        _head = GetComponent<SnakeSegment>();
 
-  private void FixedUpdate()
-  {
-    if (Time.time < _nextUpdate)
-    {
-      return;
+        if (_head != null) return;
+        _head = gameObject.AddComponent<SnakeSegment>();
+        _head.hideFlags = HideFlags.HideInInspector;
     }
 
-    if (_input != Vector2.zero)
+    private void Update()
     {
-      _head.SetDirection(_input, Vector2.zero);
+        if (_head.Direction.x != 0.0f)
+        {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+                _input = Vector2.up;
+            else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) _input = Vector2.down;
+        }
+        else if (_head.Direction.y != 0.0f)
+        {
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+                _input = Vector2.left;
+            else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) _input = Vector2.right;
+        }
     }
 
-    for (var i = _segments.Count - 1; i > 0; i--)
+    private void FixedUpdate()
     {
-      _segments[i].Follow(_segments[i - 1], i, _segments.Count);
+        if (Time.time < _nextUpdate) return;
+
+        if (_input != Vector2.zero) _head.SetDirection(_input, Vector2.zero);
+
+        for (var i = _segments.Count - 1; i > 0; i--) _segments[i].Follow(_segments[i - 1], i, _segments.Count);
+
+        var direction = _head.Direction;
+        var position = _head.transform.position;
+
+        _head.transform.position = new Vector3(
+            Mathf.Round(position.x) + direction.x,
+            Mathf.Round(position.y) + direction.y,
+            0.0f
+        );
+
+        _nextUpdate = Time.time + 1.0f / (speed * speedMultiplier);
     }
 
-    var direction = _head.Direction;
-    var position = _head.transform.position;
-
-    _head.transform.position = new Vector3(
-        Mathf.Round(position.x) + direction.x,
-        Mathf.Round(position.y) + direction.y,
-        0.0f
-    );
-
-    _nextUpdate = Time.time + 1.0f / (speed * speedMultiplier);
-  }
-
-  public bool Occupies(Vector3 position)
-  {
-    return _segments
-        .Any(s => s.transform.position.Equals(position));
-  }
-
-  private void Grow()
-  {
-    var segment = Instantiate(segmentPrefab);
-    segment.Follow(_segments.Last(), _segments.Count, _segments.Count + 1);
-
-    _segments.Add(segment);
-  }
-
-  private void ResetState()
-  {
-    _input = Vector2.zero;
-    _head.SetDirection(Vector2.right, Vector2.zero);
-    _head.transform.position = Vector3.zero;
-
-    for (var i = 1; i < _segments.Count; i++)
+    private void OnEnable()
     {
-      Destroy(_segments[i].gameObject);
+        GameManager.GameStarted.AddListener(ResetState);
     }
 
-    _segments.Clear();
-    _segments.Add(_head);
-
-    for (var i = 1; i < initialSize; i++)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-      _segments.Add(Instantiate(segmentPrefab));
+        if (other.CompareTag("Food"))
+            Grow();
+        else if (other.CompareTag("Obstacle")) IsDied?.Invoke();
     }
 
-    transform.position = Vector3.zero;
-  }
+    public bool Occupies(Vector3 position)
+    {
+        return _segments
+            .Any(s => s.transform.position.Equals(position));
+    }
 
-  private void OnTriggerEnter2D(Collider2D other)
-  {
-    if (other.CompareTag("Food"))
+    private void Grow()
     {
-      Grow();
+        var segment = Instantiate(segmentPrefab);
+        segment.Follow(_segments.Last(), _segments.Count, _segments.Count + 1);
+
+        _segments.Add(segment);
     }
-    else if (other.CompareTag("Obstacle"))
+
+    private void ResetState()
     {
-      IsDied?.Invoke();
+        _input = Vector2.zero;
+        _head.SetDirection(Vector2.right, Vector2.zero);
+        _head.transform.position = Vector3.zero;
+
+        for (var i = 1; i < _segments.Count; i++) Destroy(_segments[i].gameObject);
+
+        _segments.Clear();
+        _segments.Add(_head);
+
+        for (var i = 1; i < initialSize; i++) _segments.Add(Instantiate(segmentPrefab));
+
+        transform.position = Vector3.zero;
     }
-  }
 }
